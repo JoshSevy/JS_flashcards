5a690df5a2feec0a1ce8cf85d94f2f82f3d65eb7
Joshua Sevy <joshuasevy@outlook.com>
2020-06-29 16:11:53 -0600

    Add Turn.js file, layout for class, psudocode for the methods

---
+++ src/Turn.js
@@ -0,0 +1,22 @@
+class Turn {
+  constructor(guess, card){
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess(){
+// return this.guess
+  }
+
+  returnCard() {
+// return current card from card class
+  }
+
+  evaluateGuess() {
+// return if guess is true(correct) or false(incorrect)
+  }
+
+  giveFeedback() {
+// return either 'incorrect!' or 'correct!
+  }
+}
