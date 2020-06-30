969056d27d165b011720ee44d8b92f5b9e66c1e3
Joshua Sevy <joshuasevy@outlook.com>
2020-06-29 19:17:12 -0600

    All methods added to class and tested with turn-test.js

--- src/Turn.js
+++ src/Turn.js
@@ -2,21 +2,34 @@ class Turn {
   constructor(guess, card){
     this.guess = guess;
     this.card = card;
+    this.correct;
   }
 
-  returnGuess(){
-// return this.guess
+  returnGuess() {
+  return this.guess;
   }
 
   returnCard() {
-// return current card from card class
+  return this.card;
   }
 
   evaluateGuess() {
+    if(this.guess === this.card.correctAnswer) {
+      return this.correct = true;
+    } else {
+      return this.correct = false;
+    }
 // return if guess is true(correct) or false(incorrect)
   }
 
   giveFeedback() {
+    if(this.correct === true) {
+      return 'correct!';
+    } else {
+      return 'incorrect!';
+    }
 // return either 'incorrect!' or 'correct!
   }
-}
+};
+
+module.exports = Turn;
