03045420da7c5fe6bcf623889a8299086c540eb5
Joshua Sevy <joshuasevy@outlook.com>
2020-06-29 20:35:12 -0600

    Refactor to pass eslint, errors

--- src/Turn.js
+++ src/Turn.js
@@ -1,35 +1,35 @@
 class Turn {
-  constructor(guess, card){
+  constructor(guess, card) {
     this.guess = guess;
     this.card = card;
     this.correct;
   }
 
   returnGuess() {
-  return this.guess;
+    return this.guess;
   }
 
   returnCard() {
-  return this.card;
+    return this.card;
   }
 
   evaluateGuess() {
-    if(this.guess === this.card.correctAnswer) {
+    if (this.guess === this.card.correctAnswer) {
       return this.correct = true;
     } else {
       return this.correct = false;
     }
-// return if guess is true(correct) or false(incorrect)
-}
+    // return if guess is true(correct) or false(incorrect)
+  }
 
   giveFeedback() {
-    if(this.correct === true) {
+    if (this.correct === true) {
       return 'correct!';
     } else {
       return 'incorrect!';
     }
-// return either 'incorrect!' or 'correct!
+    // return either 'incorrect!' or 'correct!
   }
-};
+}
 
 module.exports = Turn;
