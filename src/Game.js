9051cd47cb66c5f7faecfdd29e5a39617f06a09d
Joshua Sevy <joshuasevy@outlook.com>
2020-07-01 17:10:49 -0600

    Add methods per rubric all test passing, pre refactor

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,38 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.cards = [];
+    this.currentRound;
+    this.deck;
+  }
+
+  start() {
+    this.createCards();
+    this.createDeck();
+    this.createRound();
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+
+  createCards() {
+    prototypeQuestions.forEach(card => {
+      this.cards.push(new Card(card.id,card.question, card.answers, card.correctAnswer));
+    });
+  }
+
+  createDeck() {
+    this.deck = new Deck(this.cards);
+  }
+
+  createRound() {
+    this.currentRound = new Round(this.deck);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
